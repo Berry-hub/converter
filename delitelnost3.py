e2fcf61f09ec22e6acf0b623438c3d378743c508
Patrik Bernat <patrikbernat@gmail.com>
2021-01-19 18:51:14 +0100

    Pridany novy program na delitelnost
    tento program zisti, ci je zadane cislo delitelne 3

---
+++ delitelnost3.py
@@ -0,0 +1,7 @@
+# tento program zisti, ci je zadane cislo delitelne 3
+
+cislo = int(input("zadaj akekolvek cele cislo: "))
+if cislo % 3 == 0:
+    print("ano, cislo je delitelne 3")
+else:
+    print("nie, cislo nie je delitelne 3")
