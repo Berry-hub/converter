9e24b2b7cf81875b52efb817d950e558df87eb3a
Patrik Bernat <patrikbernat@gmail.com>
2021-02-07 08:44:41 +0100

    Upravena hra kamen/papier/noznice
    teraz caka na tvoj tah a az potom ukaze tah pocitaca
    urobene dalsie mensie upravy

--- kamen_papier_noznice.py
+++ kamen_papier_noznice.py
@@ -2,28 +2,29 @@
 
 from random import randrange
 #
-print("pocitac:", end=" ")
-cislo = randrange(3)
-if cislo == 0:
-    print("kamen") 
-elif cislo == 1:
-    print("papier")
-elif cislo == 2:
-    print("noznice")
-#  
-print("ja:", end=" ")   
-cislo_2 = randrange(3)
-if cislo_2 == 0:
-    print("kamen") 
-elif cislo_2 == 1:
-    print("papier")
-elif cislo_2 == 2:
-    print("noznice")
+print("Ideme hrat hru kamen/papier/noznice." + "\n"
++ "Ak budes chces skoncit, napis 'koniec'.")
 #
-print("vysledok:", end=" ")
-if (cislo == 0 and cislo_2 == 0) or (cislo == 1 and cislo_2 == 1) or (cislo == 2 and cislo_2 == 2):
-    print("Remiza!")
-elif (cislo == 0 and cislo_2 == 1) or (cislo == 1 and cislo_2 == 2) or (cislo == 2 and cislo_2 == 0):
-    print("Vyhral som!")
-else:
-    print("Vyhral pocitac!")
+while True:
+    odpoved = input("Co chces hrat? Napis: 'kamen', 'papier' alebo 'noznice': ")
+    if odpoved == 'koniec':
+        break
+#         
+    print("pocitac:", end=" ")
+    cislo = randrange(3)
+    if cislo == 0:
+        print("kamen") 
+    elif cislo == 1:
+        print("papier")
+    elif cislo == 2:
+        print("noznice")
+    #
+    print("vysledok:", end=" ")
+    if (odpoved == 'kamen' and cislo == 0) or (odpoved == 'papier' and cislo == 1) or (odpoved == 'noznice' and cislo == 2):
+        print("Remiza!")
+    elif (odpoved == 'kamen' and cislo == 1) or (odpoved == 'papier' and cislo == 2) or (odpoved == 'noznice' and cislo == 0):
+        print("Prehral si!")
+    elif (odpoved == 'kamen' and cislo == 2) or (odpoved == 'papier' and cislo == 0) or (odpoved == 'noznice' and cislo == 1):
+        print("Vyhral si!")
+    else:
+        print("Nerozumiem tvojmu vyberu, zadaj 'kamen', 'papier' alebo 'noznice': ")
