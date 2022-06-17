6ad4e3d9cd1e6372a6d26e990158945d2b56713f
Berry <78561662+Berry-hub@users.noreply.github.com>
2022-06-17 11:01:28 +0800

    Delete test_prvocislo.py

--- test_prvocislo.py
+++
@@ -1,8 +0,0 @@
-#from prvocislo import prvocislo
-from fibonacci import fibonacci
-
-#def test_prvocislo():
-#    assert prvocislo(17) == [2,3,5,7,11,13,17]
-
-def test_fibonacci():
-    assert fibonacci(7) == [1,1,2,3,5,8,13]
