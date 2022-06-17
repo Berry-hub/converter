a0ca493e52d5d25952f5889a8f73b1eba67ea730
Berry <78561662+Berry-hub@users.noreply.github.com>
2022-06-17 10:58:38 +0800

    Delete delitelnost.py

--- delitelnost.py
+++
@@ -1,8 +0,0 @@
-# tento program zisti, ci je cislo delitelne zadanym delitelom
-
-cislo = int(input("zadaj akekolvek cele cislo: "))
-delitel = int(input("zadaj delitela: "))
-if cislo % delitel == 0:
-    print("ano, cislo je delitelne delitelom")
-else:
-    print("nie, cislo nie je delitelne delitelom")
